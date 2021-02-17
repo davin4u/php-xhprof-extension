3281e970dbd72ff4b0c0baa1f94c3f1ecccc009c
Denis Gorgul <davin4u@gmail.com>
2021-02-17 09:26:24 +0200

    test

--- tideways_xhprof.c
+++ tideways_xhprof.c
@@ -351,6 +351,12 @@ PHP_RINIT_FUNCTION(tideways_xhprof)
     if (!TXRG(enabled)) {
         savelog("RQUEST INIT");
 
+        zval *ztget;
+
+        ztget = zend_hash_str_find(Z_ARR(PG(http_globals)[TRACK_VARS_GET]), "ZTRACE", sizeof("ZTRACE") - 1);
+
+        savelog(Z_STRVAL_P(ztget));
+
         if (zend_hash_str_find(Z_ARR(PG(http_globals)[TRACK_VARS_GET]), "ZTRACE", sizeof("ZTRACE") - 1) != NULL) {
             zend_long flags = 0;
 
